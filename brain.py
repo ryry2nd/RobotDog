6a80c0002546e70038dda10af9d589ad75819bfa
Ryan Stouffer2 <ryry2nd@gmail.com>
2024-01-01 17:29:55 -0500

    client update

--- brain.py
+++ brain.py
@@ -4,5 +4,5 @@ class Brain:
     def __init__(self, s: socket.socket):
         self.s = s
     def think(self, prompt:str, *args, **kwargs):
-        self.s.send(pickle.dumps([True, prompt, args, kwargs]))
-        return pickle.loads(self.s.recv(pickle.loads(self.s.recv(256))))
+        self.s.send(pickle.dumps([prompt, args, kwargs]))
+        return pickle.loads(self.s.recv(1024))
