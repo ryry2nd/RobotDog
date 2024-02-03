e20fbe3ed1c34d95adc4ab1410c9192b5b2945b7
Ryan Stouffer2 <ryry2nd@gmail.com>
2024-02-03 14:55:32 -0500

    fixed stuff

--- servos/toFullName.py
+++ servos/toFullName.py
@@ -5,12 +5,22 @@ reversedSkillFullName = {}
 for d, data in skillFullName.items():
     reversedSkillFullName[data] = d
 
-def shortToLong(st: str):
-    return skillFullName.get(st.strip())
+def shortToLong(*st: str):
+    for s in st:
+        s = s.strip()
+        if s in skillFullName.keys():
+            return skillFullName.get(s.strip())
+    return None
 
-def longToShort(st: str):
-    output = ''.join(x for x in st.title() if x.isalnum())
-    return reversedSkillFullName.get((output[0].lower() + output[1:]))
+def longToShort(*st: str):
+    for s in st:
+        s = s.strip()
+        if s in reversedSkillFullName.keys():
+            output = ''.join(x for x in s.title() if x.isalnum())
+            return reversedSkillFullName.get((output[0].lower() + output[1:]))
 
-def isWord(st: str):
-    return st in reversedSkillFullName.keys()
+def isWord(*st: str):
+    for s in st:
+        if s.strip() in reversedSkillFullName.keys():
+            return True
+    return False
